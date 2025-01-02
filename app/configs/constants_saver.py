91abca68b7f5977ac77b3482f6d9faedef8f1e7e
VLMedvedev <c066en@gmail.com>
2025-01-02 21:36:51 +0100

    configs

--- app/configs/constants_saver.py
+++ app/configs/constants_saver.py
@@ -43,8 +43,8 @@ def is_obj(obj):
 
 class ConstansReaderWriter():
     def __init__(self, module_config_name, **kwargs):
-        self.file_config_name = f"configs/{module_config_name}.py"
-        self.obj = __import__(f"{module_config_name}")
+        self.file_config_name = f"/configs/{module_config_name}.py"
+        self.obj = __import__(module_config_name)
         self.config_dict  = self.get_constants_dict()
 
     def get_constants_dict(self):
