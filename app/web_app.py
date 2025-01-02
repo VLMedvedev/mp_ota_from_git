ff216f7973bffbb8404a1fa35e36e2515f3d83d0
VLMedvedev <c066en@gmail.com>
2025-01-02 11:41:26 +0100

    web_app

--- app/web_app.py
+++ app/web_app.py
@@ -21,7 +21,7 @@ def application_mode():
 
     def app_index(request):
         #return render_template(f"{APP_TEMPLATE_PATH}/home.html")
-        return render_template(f"index2.html",
+        return render_template(f"{APP_TEMPLATE_PATH}/index2.html",
                                title=AP_NAME,
                                style_css_str=CSS_STYLE, )
 
@@ -51,35 +51,35 @@ def application_mode():
         os.remove(WIFI_FILE)
         # Reboot from new thread after we have responded to the user.
         _thread.start_new_thread(machine_reset, ())
-        return render_template(f"reset.html", access_point_ssid=AP_NAME)
+        return render_template(f"{APP_TEMPLATE_PATH}/reset.html", access_point_ssid=AP_NAME)
 
     def app_reboot(request):
         # Reboot from new thread after we have responded to the user.
         _thread.start_new_thread(machine_reset, ())
-        return render_template(f"reboot.html", access_point_ssid=AP_NAME)
+        return render_template(f"{APP_TEMPLATE_PATH}/reboot.html", access_point_ssid=AP_NAME)
 
     def app_catch_all(request):
         return "Not found.", 404
 
     def about(request):
-        return render_template(f"about.html",
+        return render_template(f"{APP_TEMPLATE_PATH}/about.html",
                                title="About this Site",
                                style_css_str=CSS_STYLE, )
 
     def login_form(request):
         print(request.method)
         if request.method == 'GET':
-            return render_template(f"login.html")
+            return render_template(f"{APP_TEMPLATE_PATH}/login.html")
         if request.method == 'POST':
             username = request.form.get("username", None)
             password = request.form.get("password", None)
 
             if username == "vladimir" and password == "password":
-                return render_template(f"default.html",
+                return render_template(f"{APP_TEMPLATE_PATH}/default.html",
                                        content=f"<h1>Welcome back, {username}</h1>",
                                        style_css_str=CSS_STYLE)
             else:
-                return render_template(f"default.html",
+                return render_template(f"{APP_TEMPLATE_PATH}/default.html",
                                        content="Invalid username or password",
                                        title="About this Site",
                                        style_css_str=CSS_STYLE)
