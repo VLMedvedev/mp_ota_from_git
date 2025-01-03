0de67b89aad03cbed56e224c6950b1e494d65701
VLMedvedev <c066en@gmail.com>
2025-01-03 10:24:00 +0100

    web configs

--- app/app_templates/web_app.py
+++ app/app_templates/web_app.py
@@ -120,7 +120,7 @@ def application_mode():
         if request.method == 'POST':
             config_page_dict = request.form
             print(config_page_dict)
-           # crw.set_constants_from_config_dict(config_page_dict)
+            crw.set_constants_from_config_dict(config_page_dict)
             return render_template("/app_templates/config_page.html",
                                    config_page=config_page,
                                    page_info="Params saved !!!",
