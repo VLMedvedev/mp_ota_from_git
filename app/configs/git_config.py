743557a3aa9d552f9d7558901afa21985168adee
VLMedvedev <c066en@gmail.com>
2025-01-04 00:43:29 +0100

    refactoring

--- app/configs/git_config.py
+++ app/configs/git_config.py
@@ -3,6 +3,6 @@ GITHUB_REPOSITORY = 'mp_ota_from_git'
 GITHUB_BRANCH = 'main'
 GITHUB_APP_FOLDER = 'app'
 GITHUB_TOKEN = ""
-EXCLUDE_LIST = ["sys_config.py", "git_config.py", "wifi_ap_config.py", "sha1_internal_save_file.json"]
+EXCLUDE_LIST = "sys_config.py,git_config.py,wifi_ap_config.py,sha1_internal_save_file.json"
 AUTO_UPDATE_FROM_GIT = False
 REBUILD_SHA1_INTERNAL_FILE = False
