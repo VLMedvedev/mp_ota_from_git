6e4cfe6850fdce9e354ec4e98e0ed40f29f80ab4
VLMedvedev <c066en@gmail.com>
2025-01-04 10:02:56 +0100

    refactoring

--- app/configs/git_config.py
+++ app/configs/git_config.py
@@ -4,5 +4,5 @@ GITHUB_BRANCH = 'main'
 GITHUB_APP_FOLDER = 'app'
 GITHUB_TOKEN = ""
 EXCLUDE_LIST = "configs/sys_config.py,configs/git_config.py,configs/wifi_ap_config.py,sha1_internal_save_file.json"
-AUTO_UPDATE_FROM_GIT = False
+AUTO_UPDATE_FROM_GIT = True
 REBUILD_SHA1_INTERNAL_FILE = False
