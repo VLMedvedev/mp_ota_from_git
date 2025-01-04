88120b47bfa7c888ee73da11c91a8f292350f7b0
VLMedvedev <c066en@gmail.com>
2025-01-04 14:45:48 +0100

    refactoring

--- app/configs/hw_config.py
+++ app/configs/hw_config.py
@@ -2,8 +2,6 @@ HW_LED_PIN = 15
 HW_RELE_PIN = 16
 HW_CAN_RX_PIN = 37
 HW_CAN_TX_PIN = 39
-# bt_min_up bt_max_up        12   7
-# bt_min_down bt_max_down    11   9
 HW_BT_LEFT_UP = 12
 HW_BT_LEFT_DOWN = 11
 HW_BT_RIGTH_UP = 7
