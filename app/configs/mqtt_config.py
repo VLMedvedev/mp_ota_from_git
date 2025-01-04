6d0eaab84ed58391c0af924a70c4097a8cfd83f3
VLMedvedev <c066en@gmail.com>
2025-01-04 17:10:03 +0100

    refactoring

--- app/configs/mqtt_config.py
+++ app/configs/mqtt_config.py
@@ -1,7 +1,9 @@
-MQTT_SERVER = "ip address"
-MQTT_PORT = 1880
-MODE = "AUTO"
-
-
+AUTO_START_MQTT = False
+SERVER="broker.emqx.io"
+CLIENTID = 'test-pub-1vnbxgf943285769'
+USER = "emqx"
+PASSWORD = "public"
+TOPIC = "test-pub/mqtt"
+MSG = "hello"
 
 
