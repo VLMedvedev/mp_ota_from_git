3136a9f25e606b066e758786f3c7632a72573b66
VLMedvedev <c066en@gmail.com>
2025-01-05 11:56:21 +0100

    mqtt

--- app/mp_mqtt.py
+++ app/mp_mqtt.py
@@ -1,3 +1,16 @@
+import threading
+import time
+
+from umqtt import sub_led, pub_button
+import _thread
 
 def main():
     print("start mqtt")
+    sub_mqtt_th = threading.Thread(target=sub_led, args=())
+    pub_mqtt_th = threading.Thread(target=pub_button, args=())
+    sub_mqtt_th.start()
+    pub_mqtt_th.start()
+    while True:
+        print("wait")
+        time.sleep(5)
+
