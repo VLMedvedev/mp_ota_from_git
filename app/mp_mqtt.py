ca5909fceedd2e66156627a7de8d4eff7a2d2ee5
VLMedvedev <c066en@gmail.com>
2025-01-05 12:54:20 +0100

    mqtt

--- app/mp_mqtt.py
+++ app/mp_mqtt.py
@@ -5,9 +5,9 @@ import _thread
 
 def main():
     print("start mqtt")
-    sub_mqtt_th = _thread.start_new_thread(sub_main, ())
-    #pub_mqtt_th = _thread.start_new_thread(pub_main, ())
-    pub_main()
+    #sub_mqtt_th = _thread.start_new_thread(sub_main, ())
+    pub_mqtt_th = _thread.start_new_thread(pub_main, ())
+    #pub_main()
    # while True:
     print("wait")
     time.sleep(5)
